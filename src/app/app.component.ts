d370e5ef1ebb3cd79d7b4cb3f06265d99b55419d
Oliver Veits <oliver.veits@gmail.com>
2018-04-06 23:44:03 +0200

    added DomSanitizer to trust HTML and to make table of content links functional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-root',
@@ -7,17 +9,17 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  public title : String = 'Loading title...';
-  public content : String = 'Loading content...';
+  public title : SafeHtml|String = 'Loading title...';
+  public content : SafeHtml|String = 'Loading content...';
 
-  constructor(private _http: HttpClient) {
+  constructor(private _http: HttpClient, private _sanitizer: DomSanitizer) {
   }
 
   ngOnInit() {
      this._http.get('https://public-api.wordpress.com/rest/v1.1/sites/vocon-it.com/posts/3078')
                  .subscribe(data => {
-                        this.title = data.title;
-                        this.content = data.content;
+                        this.title = this._sanitizer.bypassSecurityTrustHtml(data.title);
+                        this.content = this._sanitizer.bypassSecurityTrustHtml(data.content);
                         console.log(data);
                 });
   }
