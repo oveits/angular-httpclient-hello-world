a48e1ad677bcc1618739b81b48f60d305ed89f80
Oliver Veits <oliver.veits@gmail.com>
2018-04-06 23:10:58 +0200

    simple get displaying title and content of the post

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  public post : WordPressPosts = null;
+  public title : String = 'Loading title...';
+  public content : String = 'Loading content...';
+
+  constructor(private _http: HttpClient) {
+  }
+
+  ngOnInit() {
+     this._http.get('https://public-api.wordpress.com/rest/v1.1/sites/vocon-it.com/posts/3078')
+                 .subscribe(data => {
+                        this.title = data.title;
+                        this.content = data.content;
+                        console.log(data);
+                });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
 
 
 import { AppComponent } from './app.component';
@@ -10,6 +12,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule
   ],
   providers: [],
